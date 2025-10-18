ec6415a92ab405881de7d427817cec6e8e182828
beacmc <kornieiko1999@gmail.com>
2025-10-18 17:54:38 +0300

    Add parse placeholders method on button

--- api/src/main/java/com/beacmc/beacmcauth/api/social/keyboard/button/Button.java
+++ api/src/main/java/com/beacmc/beacmcauth/api/social/keyboard/button/Button.java
@@ -15,4 +15,15 @@ public class Button {
     String label;
     String callbackData;
     ButtonType type;
+
+    public Button parsePlaceholders(Map<String, ?> placeholders) {
+        Button button = new Button(label, callbackData, type);
+        if (placeholders != null) {
+            for (Map.Entry<String, ?> entry : placeholders.entrySet()) {
+                button.setLabel(button.getLabel().replace(entry.getKey(), String.valueOf(entry.getValue())));
+                button.setCallbackData(button.getCallbackData().replace(entry.getKey(), String.valueOf(entry.getValue())));
+            }
+        }
+        return button;
+    }
 }
