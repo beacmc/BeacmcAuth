b641233b44e24b94fe64dff3f2565d6a1e86b58c
beacmc <kornieiko1999@gmail.com>
2025-10-18 17:08:01 +0300

    refactor and add "inline" support

--- api/src/main/java/com/beacmc/beacmcauth/api/social/keyboard/Keyboard.java
+++ api/src/main/java/com/beacmc/beacmcauth/api/social/keyboard/Keyboard.java
@@ -2,16 +2,19 @@ package com.beacmc.beacmcauth.api.social.keyboard;
 
 import com.beacmc.beacmcauth.api.social.keyboard.button.Button;
 import lombok.*;
+import lombok.experimental.Accessors;
 
 import java.util.List;
 
 @Getter
 @Setter
+@Builder
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
-@Builder
+@Accessors(chain = true)
 public class Keyboard {
 
     List<List<Button>> buttons;
+    boolean inline;
 }
