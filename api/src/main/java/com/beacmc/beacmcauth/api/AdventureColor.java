dee8ac714edad5720febf29b4dee6fbb41241121
beacmc <kornieiko09@gmail.com>
2026-03-27 18:01:26 +0200

    Move AdventureColor to 'api'

---
+++ api/src/main/java/com/beacmc/beacmcauth/api/AdventureColor.java
@@ -0,0 +1,14 @@
+package com.beacmc.beacmcauth.api;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+public class AdventureColor {
+
+    public static Component of(String content) {
+        return LegacyComponentSerializer.legacyAmpersand()
+                .deserialize(content)
+                .decoration(TextDecoration.ITALIC, false);
+    }
+}
